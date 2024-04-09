8e5e92980ccfdba5e8aff07efb42c05e4f27af59
Madeleine Min <mmin@qualtrics.com>
2024-04-08 18:28:37 -0700

    fix(home): update bio and packages

--- src/components/ContactComponent/index.js
+++ src/components/ContactComponent/index.js
@@ -7,9 +7,8 @@ const ContactComponent = () => {
             <ContactText>
                 <h3>beep me if you want to reach me!</h3>
                 email: maddiem2018@gmail.com <br></br> 
-                linkedout: <a href='https://www.linkedin.com/in/madeleine-min'>://</a><br></br>
+                linkedout: <a href='https://www.linkedin.com/in/maddiemin'>://</a><br></br>
                 github: <a href='https://github.com/madeleinemin8'>@madeleinemin8</a><br></br>
-                instagram: <a href='https://www.instagram.com/maddiemin/'>@maddiemin</a>
             </ContactText>
         </ContactContainer>
     );
--- src/components/HomeComponent/index.js
+++ src/components/HomeComponent/index.js
@@ -4,12 +4,12 @@ import {AboutText, HomeContainer, MeImage, MushroomImage} from "./HomeComponentE
 const HomeComponent = () => {
     return (
         <HomeContainer>
-            <AboutText>Hi, I'm Maddie Min, a software development engineer currently residing in Brooklyn, NY.
-                I grew up in the DC area and spent some time in Chicago for my undergrad before moving to the city. 
-                I'm interested in iOS development, web development (live laugh love React), and have experience with scale testing as well.
+            <AboutText>Hi, I'm Maddie Min, a software dev residing in Seattle, WA.
+                I grew up in the DC area and spent some time in Chicago for my undergrad (Go Bears). 
+                I'm currently working as a full-stack engineer making tools for UI, scale, and resiliency testing.
                 Outside of work, you can find me hanging out outside, biking, running, picnicking,
-                 and making digital and analog art. I listen to r&b, soul, house, italo disco, and sometimes techno. 
-                 I also love traveling, trying out new restaurants, and exploring museums and galleries.
+                 and making digital and analog art. I listen to r&b, house, and sometimes techno and folk.
+				 I also love traveling, trying out new restaurants, and exploring museums.
             </AboutText>
             <MeImage></MeImage>
             <MushroomImage></MushroomImage>
